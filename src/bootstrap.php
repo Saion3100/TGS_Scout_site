5c35b0ecd034f3bb5d527ec6fcaf1c444d0dba0d
田松彩音 <tamattyandao1203@gmail.com>
2026-07-24 15:38:45 +0900

    refactor: KRCロゴをタブに追加

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -89,6 +89,7 @@ function renderHeader(string $title = '', string $current = ''): void
 <body>
 <header class="site-header">
     <a class="brand" href="/" aria-label="TGS Scout ホーム">
+        <img class="brand-logo" src="/assets/KRClogo.jpg" alt="KRC">
         <span class="brand-mark">TGS</span><span>SCOUT</span><small>2026</small>
     </a>
     <button class="nav-toggle" type="button" aria-expanded="false" aria-controls="global-nav">MENU</button>
